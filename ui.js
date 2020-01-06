789b5c5454b91d4111e59f5d5b89abbafab7b16d
Joonas Salonpää <joonas1000@gmail.com>
2020-01-06 12:36:36 +0200

    very sketchy implementation of oscilloscope pong (left channel to X input and right channel to Y input)

---
+++ ui.js
@@ -0,0 +1,58 @@
+let processor
+const start = async (givenProjectId) => {
+
+    if (processor) return;
+        
+    const audioCtx = new AudioContext(/*{
+        latencyHint: 'playback'
+    }*/)
+    await audioCtx.audioWorklet.addModule('xy-drawer.js')
+    processor = new AudioWorkletNode(audioCtx, 'xy-drawer', { outputChannelCount: [2] })
+    await processor.connect(audioCtx.destination)
+    processor.port.postMessage({resolution: {w: 200, h: 200}})
+    
+    document.body.onkeydown = event => {
+        const c = event.code.replace('Key', '')
+        if (c === 'X') {
+            paddleADir = -1
+        }
+        if (c === 'Z') {
+            paddleADir = 1
+        }
+        if (c === 'M') {
+            paddleBDir = -1
+        }
+        if (c === 'N') {
+            paddleBDir = 1
+        }
+    }
+    setInterval(() => {
+        if (paddleADir < 0 && paddleA > 0) paddleA -= 4
+        if (paddleADir > 0 && paddleA < 199) paddleA += 4
+        if (paddleBDir < 0 && paddleB > 0) paddleB -= 4
+        if (paddleBDir > 0 && paddleB < 199) paddleB += 4
+        ball[0] += ballDir[0] * 3
+        ball[1] += ballDir[1] * 3
+        if (ball[0] > 199) {ball[0] = 199; ballDir[0] *= -1}
+        else if (ball[0] < 0) {ball[0] = 0; ballDir[0] *= -1}
+        else if (ball[1] > 185 && ballDir[1] === 1 && Math.abs(paddleA - ball[0]) < 32) {ballDir[1] *= -1;ballDir[0] = Math.random() < 0.5 ? -1 : 1;}
+        else if (ball[1] < 15 && ballDir[1] === -1 && Math.abs(paddleB - ball[0]) < 32) {ballDir[1] *= -1;ballDir[0] = Math.random() < 0.5 ? -1 : 1;}
+        else if (ball[1] < 5) {ball[1] = 195; ball[0]=paddleA; ballDir = [0,-1]}
+        else if (ball[1] > 195) {ball[1] = 15; ball[0]=paddleB; ballDir = [0,1]}
+        
+        if (processor) {
+            processor.port.postMessage({pixels: [
+                ...new Array(48/2).fill(0).map((_,i) => ({ x: paddleA + Math.floor(i/2) - 12, y: 190 + i%2 })),
+                ...new Array(48/2).fill(0).map((_,i) => ({ x: paddleB + Math.floor(i/2) - 12, y: 10 + i%2 })),
+                ...new Array(32/2).fill(0).map((_,i) => ({ x: ball[0] + Math.floor(i/2) - 8, y: ball[1] + i%2 })),
+            ]})
+        }
+    }, 25)
+}
+
+let paddleA = 100
+let paddleB = 100
+let ball = [100,100]
+let ballDir = [1, 1]
+let paddleADir = 0
+let paddleBDir = 0
---
+++ xy-drawer.js
@@ -0,0 +1,41 @@
+class XyDrawer extends AudioWorkletProcessor {
+    
+    constructor() {
+        super()
+        this.port.onmessage = e => this.handlePortEvent(e.data)
+        this.pixels = []
+        this.resolution = {w: 100, h: 100}
+    }
+    
+    handlePortEvent(data) {
+        if (data.resolution !== undefined) {
+            this.resolution = data.resolution
+        } else if (data.pixels !== undefined) {
+            this.pixels = [
+                ...data.pixels.map(px => ({ x: -0.1-0.9*px.x / this.resolution.w, y: -0.1-0.9*px.y / this.resolution.h })),
+                ...data.pixels.map(px => ({ x: 0.1+0.9*px.x / this.resolution.w, y: 0.1+0.9*px.y / this.resolution.h })),            
+            ]
+        }
+    }
+    
+    process (inputs, outputDevices, parameters) {
+        if (this.pixels.length === 0) return true
+        
+        const dev1OutputChannels = outputDevices[0]
+        const channel1LeftBuffer = dev1OutputChannels[1]
+        const channel1RightBuffer = dev1OutputChannels[0]
+        
+        let pxidx = 0
+        
+        for (let i = 0; i < channel1LeftBuffer.length; i++) {
+            if (pxidx === this.pixels.length) pxidx = 0;
+            channel1LeftBuffer[i] = this.pixels[pxidx].y
+            channel1RightBuffer[i] = this.pixels[pxidx].x
+            pxidx++
+        }
+        return true
+    }
+
+}
+
+registerProcessor('xy-drawer', XyDrawer)
